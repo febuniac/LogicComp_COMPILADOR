d1beedfdbe32216a72ea660487436b5ca8643719
Felipe Buniac <febuniac@gmail.com>
2018-08-20 10:14:48 -0300

    Segunda entrega: Tokenizador e Analisador de expressões matemáticas

--- compilador_BUNIAC.py
+++ compilador_BUNIAC.py
@@ -1,47 +1,101 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+#os três tipos que um token pode ter (constantes)
+INT = "INT"
+PLUS = "PLUS"
+MINUS = "MINUS"
+EOF = "EOF" #end of file
+ERROR ="ERRO"
 
-#Analisa a string de Input do usuário e joga em uma lista o que são digitos e o que são operadores
-def analise(math_op,lista_num,lista_ops):
-    digoff ="" #inicializando o digoff como uma string vazia
-    # Loop over string.
-    for i in math_op:
-        if i.isdigit():
-            digoff+=i #salvando digito em uma variavel para o caso de numeros com 1+ digitos
-            #print("nums",lista_num)
-        elif i == "+" or i == "-":
-            lista_ops.append(i)
-            x = int(digoff) #conversão da string do digito para um int
-            lista_num.append(x)
-            digoff=""#limapndo o digoff para estar limpo quando for add um novo num
-            #print("ops",lista_ops)
-        elif i == " ":
-            continue
+#Classe Token
+
+class Token:
+    def __init__(self,tipo,valor):
+        self.tipo=tipo#tipo (string-tipo do token);
+        self.valor=valor#valor (integer-valor do token);
+
+#Classe Tokenizador
+
+class Tokenizador:
+    def __init__(self,origem):
+        self.origem = origem#origem (string-codigo fonte tokenizado)-> conta matemática(entrada do usuario)
+        self.posicao = 0#posição (integer-posição atual que tokenizador irá separar);
+        self.atual = Token(EOF,EOF)#atual (token-ultimo token separado);
+    #lê o próximo token e atualiza o atributo atual
+    def selecionarProximo(self):
+        digito =""#Para numeros com mais de 1 digito
+
+        #sempre aqui pois pega um token de cada vez
+        while self.posicao < len(self.origem) and (self.origem[self.posicao] == " "):#limpando os espaços
+            self.posicao+=1#atualiza a posição
+
+        if self.posicao >= len(self.origem):#checa o tamanho da string de entrada
+            token = Token(EOF,'null')#Para o fim da string
+            self.atual=token#atualiza o atual
+
+        elif (self.origem[self.posicao]).isdigit():#se for digito
+            while (self.posicao<(len(self.origem)) and (self.origem[self.posicao]).isdigit()):
+                digito +=self.origem[self.posicao]
+                self.posicao+=1
+
+            token = Token(INT,int(digito))
+            self.atual=token
+
+        elif self.origem[self.posicao] == '+':
+            token = Token(PLUS,"+")
+            self.posicao+=1
+            self.atual=token
+
+        elif self.origem[self.posicao] == '-':
+            token = Token(MINUS,"-")
+            self.posicao+=1
+            self.atual=token
+
+
+
+#Classe Analisador(estática)
+#tokens (Tokenizador-ler código fonte e alimentar o Analisador)
+class Analisador:
+    tokens = None
+    #consome tokens do Tokenizador e análisa se a sintaxe está aderente à grámatica proposta
+    def inicializar(texto):
+        Analisador.tokens = Tokenizador(texto)#inicializa o atributo Tokenizador dentro da classe Analisador(classe estatica por isso é necessário)
+
+    def analisarExpressao():
+        resultado = 0
+        Analisador.tokens.selecionarProximo()#first
+        if Analisador.tokens.atual.tipo == INT:
+            resultado = Analisador.tokens.atual.valor
+            Analisador.tokens.selecionarProximo()
+
+            while (Analisador.tokens.atual.tipo != EOF):
+
+                if (Analisador.tokens.atual.tipo == PLUS):
+                    Analisador.tokens.selecionarProximo()
+
+                if (Analisador.tokens.atual.tipo == INT):
+                    resultado+=Analisador.tokens.atual.valor
+
+                elif (Analisador.tokens.atual.tipo == MINUS):
+                    Analisador.tokens.selecionarProximo()
+
+                    if (Analisador.tokens.atual.tipo == INT):
+                        resultado+=Analisador.tokens.atual.valor
+                else:
+                    raise Exception("Token não esperado:Deveria ser operador e veio número")
+                Analisador.tokens.selecionarProximo()
         else:
-            print("Isto não faz parte da Matemática")
-            break
-    x = int(digoff) #conversão da string do digito para um int
-    lista_num.append(x)#colocando o ultimo dig da oeperacao na lista como um numero
+            raise Exception("Token não esperado:Deveria ser numero e veio algo diferente")
+        return resultado
 
-#Pega os digitos em ordem, checa a operação e faz a operação matemática
-def operations(num1,num2,operador):# fazer recursivo 1+1 resultado  depoius resultado +1
-    if operador ==  "+" :
-        resultado = num1 + num2
-    elif operador ==  "-" :
-        resultado = num1 - num2
-    return resultado
-#resultado operacao num 3
+def main():
+    try:
+        x = "1+      2+40+83"
+        Analisador.inicializar(x)
+        print(Analisador.analisarExpressao())
 
-math_op = (str(input("Enter a math operation:")))#Entrada do usuário
-lista_num=[] #lista de digitos
-lista_ops=[] #lista de operadores
-analise(math_op,lista_num,lista_ops)
-while len(lista_num)>1:#o ultimo valor empilhado é o resultado final
-    num1 = lista_num.pop(0)#primeiro digito da operação
-    print("primeiro", num1)
-    num2 = lista_num.pop(0)#segundo digito da operação
-    print("segundo", num2)
-    operador = lista_ops.pop(0) #operador da operação
-    res = operations(num1,num2,operador)
-    lista_num.insert(0,res)#insere o resultado na posic'ão da lista (posição 0 = topo
-print("Resultado=", lista_num[0])#printa o resukltado apenas no final
+    except Exception as erro:
+        print(erro)
+
+if __name__== "__main__":
+    main()
